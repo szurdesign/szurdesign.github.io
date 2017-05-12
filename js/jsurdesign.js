c862c0f03c3602454ff62d6baf2900957a9a6dec
urdesign <470381959@qq.com>
2017-05-12 09:15:40 +0800

    第八次尝试载入图片加入

--- js/jsurdesign.js
+++ js/jsurdesign.js
@@ -18,7 +18,7 @@ function showPic(index) {
     var picLoadTime=setInterval(function() {
         if(pic.complete) {
             if(document.getElementById("loadingPic")) {
-                loadingPic.parent.removeChild(loadingPic);
+                midPicsDivremoveChild(loadingPic);
                 midPicsDiv.appendChild(pic);
             }
             var num=0;
@@ -37,7 +37,7 @@ function showPic(index) {
         }else { //将loading的小图放上去
             if(document.getElementById("loadingPic")) {
             }else {
-                pic.parent.removeChild(pic);
+                midPicsDiv.removeChild(pic);
                 var loadingPic = new Image;
                 loadingPic.src="images/loading.gif";
                 loadingPic.id="loadingPic";
