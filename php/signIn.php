4a5dfe1d28ac25a683e23514e446465309c339de
urdesign <470381959@qq.com>
2017-06-01 11:49:04 +0800

    登录基础功能

---
+++ php/signIn.php
@@ -0,0 +1,25 @@
+<?php
+if(isset($_POST["submit"]) && $_POST["submit"]=="Sign in") {
+    // define variations
+    $phoneNumber=$_POST["phoneNumber"];
+    $password=$_POST["password"];
+
+    // database
+    $con=mysql_connect("localhost","bran","liaomelo15@");
+    mysql_select_db("szurdesign");
+    if(mysqli_connect_errno($con)) {
+        // check connection
+        echo "<script>alert('Fail to connect to MySQL Database')</script>";
+    }
+    $sql="SELECT username FROM userInfo WHERE phoneNumber='$phoneNumber' and password='$password'";
+    $result=mysql_query($sql) or die(mysql_error());
+    $num=mysql_num_rows($result);
+    if($num && $num==1) {
+        echo "<script>alert('登录成功！')</script>";
+    }else {
+        echo "<script>alert('登录失败！')</script>";
+    }
+}else {
+    echo "<script>alert('登录失败！')</script>";
+}
+?>
