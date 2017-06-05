213fd70e361096c71fdbebcae50540ad21fbe02c
bran <liaminscnu@outlook.com>
2017-06-05 22:26:53 +0800

    signOut.php modified

--- php/signOut.php
+++ php/signOut.php
@@ -1,10 +1,10 @@
 <?php
-echo 'connected!';
 session_start();
-if(isset($_SESSION['usrename'])) {
+if(! isset($_SESSION['usrename'])) {
     unset($_SESSION['username']);
     session_destroy();
     echo '已退出登录！';
+    exit();
 }else {
     echo '尚未登录！';
 }
